d20f5c19a53ac7e69b5c000042a2cc297e43980c
Goofran Shaikh <ext02@dispritv.com>
2024-08-17 23:10:54 +0530

    redis implementation on permission validator

--- middleware/permissionValidation.js
+++ middleware/permissionValidation.js
@@ -1,15 +1,24 @@
 const RolePermissionMapping = require('../models/RolePermissionMapping'); 
-
+const redisClient = require('../config/redisClient');
 const checkPermission = (requiredPermissions) => {
     return async (req, res, next) => {
         try {
+            let rolePermissions;
             const userRoleId = req.body.RoleId; 
             if (!userRoleId) {
                 return res.status(403).json({ error: 'Access denied. No role assigned.' });
             }
 
-            // Fetch the permissions for the user's role
-            const rolePermissions = await RolePermissionMapping.findOne({ RoleId: userRoleId }).populate('Permission_ids');
+            let cachedResult = await redisClient.get(`mapping-${userRoleId}`)
+            rolePermissions = JSON.parse(cachedResult);
+            console.log('Fetched from redis')
+            if(!rolePermissions || rolePermissions == null || rolePermissions == undefined){
+                // Fetch the permissions for the user's role
+                rolePermissions = await RolePermissionMapping.findOne({ RoleId: userRoleId }).populate('Permission_ids');
+                await redisClient.set(`mapping-${userRoleId}`,JSON.stringify(rolePermissions)); //save to redis after gettig response
+                console.log('Fetched from db')
+
+            }
             console.log(rolePermissions,'rolePermissions')
             const userPermissions = rolePermissions.Permission_ids.map(permission => permission.PermissionName); 
 
